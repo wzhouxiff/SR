b9af1bd9e3a7fb582df382210ff1059d00759d23
wzhouxiff <wzhoux@mail2.sysu.edu.cn>
2018-07-03 11:07:09 +0800

    Add test.sh

---
+++ test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Path to Images
+ImagePath=""
+# Path to object boxes
+BoxesPath="Path/SR/objects/PISC_objects/"
+# Path to test list
+TestList="Path/SR/list/PISC_fine_level_test.txt"
+# Path to adjacency matrix
+AdjMatrix="Path/SR/adjacencyMatrix/PISC_fine_level_matrix.npy"
+# Number of classes
+num=6
+# Path to save scores
+ResultPath=""
+# Path to model
+ModelPath="Path/SR/models/PISC_fine_level.pth.tar"
+echo $ModelPath
+
+python test.py $ImagePath $BoxesPath $TestList --weight $ModelPath --adjacency-matrix $AdjMatrix -n $num -b 1 --print-freq 100 --write-out --result-path $ResultPath
+
